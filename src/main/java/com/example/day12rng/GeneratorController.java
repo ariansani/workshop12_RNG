1f6495569fd1158cc3869aef9161310900fe2b68
Arian Sani <ariansani@hotmail.com>
2022-03-02 10:35:06 +0800

    Added images and serverside error handling

--- src/main/java/com/example/day12rng/GeneratorController.java
+++ src/main/java/com/example/day12rng/GeneratorController.java
@@ -3,6 +3,7 @@ package com.example.day12rng;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -12,8 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.day12rng.RandomNumberException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class GeneratorController {
+  private Logger logger = LoggerFactory.getLogger(GeneratorController.class);
+
 
   @GetMapping("/generator")
   public String generatorForm(Model model) {
@@ -34,9 +42,44 @@ public class GeneratorController {
 
 
       long numbersNeeded = generator.getId();
-      if (numbersNeeded > 30) {
-        throw new IllegalArgumentException("Can't ask for more numbers than are available");
+      if (numbersNeeded > 30 || numbersNeeded < 0) {
+        throw new RandomNumberException();
       }
+      String[] imgNumbers = {
+        "number0.jpg", 
+        "number1.jpg",
+        "number2.jpg",
+        "number3.jpg",
+        "number4.jpg",
+        "number5.jpg",
+        "number6.jpg",
+        "number7.jpg",
+        "number8.jpg",
+        "number9.jpg",
+        "number10.jpg",
+        "number11.jpg",
+        "number12.jpg",
+        "number13.jpg",
+        "number14.jpg",
+        "number15.jpg",
+        "number16.jpg",
+        "number17.jpg",
+        "number18.jpg",
+        "number19.jpg",
+        "number20.jpg",
+        "number21.jpg",
+        "number22.jpg",
+        "number23.jpg",
+        "number24.jpg",
+        "number25.jpg",
+        "number26.jpg",
+        "number27.jpg",
+        "number28.jpg",
+        "number29.jpg",
+        "number30.jpg",
+      };
+      List<String> selectedImg = new ArrayList<String>();
+
       Random rng = new Random();
       // Note: use LinkedHashSet to maintain insertion order
       Set<Integer> generated = new LinkedHashSet<Integer>();
@@ -44,18 +87,19 @@ public class GeneratorController {
         Integer next = rng.nextInt(30) + 1;
         // As we're adding to a set, this will automatically do a containment check
         generated.add(next);
+        selectedImg.add(imgNumbers[next]);
       }
 
       //Change the Set to a List so that we can sort it
       ArrayList<Integer> sortedGenerated = new ArrayList<>(generated);
       Collections.sort(sortedGenerated);
 
-      
+      model.addAttribute("randNumsResult",selectedImg.toArray());
       model.addAttribute("listOfRandomNumbers",sortedGenerated);
 
       return "result";
-    } catch (Exception ex) {
-      System.out.println(ex);
+    } catch (RandomNumberException ex) {
+      model.addAttribute("errorMessage", "Only accept numbers between 0 and 30(inclusive)");
       return "error";
     }
 
---
+++ src/main/java/com/example/day12rng/RandomNumberException.java
@@ -0,0 +1,5 @@
+package com.example.day12rng;
+public class RandomNumberException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+    
+}
